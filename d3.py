0007f6465c53035f079b274ad902b8ccb79c4da9
Geetika Sharma <86939815+GeetikaSh@users.noreply.github.com>
2022-09-21 11:26:34 +0100

    Add files via upload

--- d3.py
+++ d3.py
@@ -34,16 +34,8 @@ from keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
-page_bg_img =
-<style>
-body {
-background-image: url("https://www.fool.com/investing/2021/01/09/3-stocks-that-are-absurdly-cheap-right-now/");
-background-size: cover;
-}
-</style>
 
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
 # Project Details
 st.title("Time Series Forecasting")
 st.markdown("Comparitive Analysis of various Time Series models on GOOGLE Stock Closing Price")
@@ -55,11 +47,69 @@ df = yf.download('GOOG',
                       start='2018-01-01', 
                       end='2022-01-01', 
                       progress=False)
-select=st.sidebar.selectbox("Objective:",("Analysis and Risk in Investment","Invest","Model"))
+select=st.sidebar.selectbox("Objective:",("Risk in Investment","Investment Period","Model and Accuracy"))
 
 
 
-if select=="Analysis and Risk in Investment":
+
+
+def lstm(df):
+  scaler=MinMaxScaler(feature_range=(0,1))
+  df1=scaler.fit_transform(np.array(df).reshape(-1,1))
+  train_size=int(len(df1)*0.65)
+  test_size=len(df1)-train_size
+  train_data,test_data=df1[0:train_size,:],df1[train_size:len(df1),:1]
+  def create_dataset(dataset,time_step=1):
+    dataX,dataY= [],[]
+    for i in range(len(dataset)-time_step-1):
+      a=dataset[i:(i+time_step),0]
+      dataX.append(a)
+      dataY.append(dataset[i+time_step,0])
+    return np.array(dataX), np.array(dataY)
+  time_step=100
+  X_train,y_train= create_dataset(train_data,time_step)
+  X_test,y_test=create_dataset(test_data,time_step)
+  X_train= X_train.reshape(X_train.shape[0],X_train.shape[1],1)
+  X_test= X_test.reshape(X_test.shape[0],X_test.shape[1], 1)
+  model=Sequential()
+  model.add(LSTM(100, return_sequences=True, input_shape=(100,1)))
+  model.add(LSTM(1000,return_sequences=False))
+  model.add(Dense(25))
+  model.add(Dense(1))
+  model.compile(loss='mean_squared_error',optimizer='adam')
+  model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=10,batch_size=64,verbose=1)
+  train_predict=model.predict(X_train)
+  test_predict=model.predict(X_test)
+  train_predict=scaler.inverse_transform(train_predict)
+  test_predict=scaler.inverse_transform(test_predict)
+  look_back=100
+  trainPredictPlot=np.empty_like(df1)
+  trainPredictPlot[:,:]=np.nan
+  trainPredictPlot[look_back:len(train_predict)+look_back,:]=train_predict
+    # shift test predictions for plotting
+  testPredictPlot=np.empty_like(df1)
+  testPredictPlot[:,:]=np.nan
+  testPredictPlot[len(train_predict)+(look_back*2)+1:len(df1)-1,:]=test_predict
+  return(df1,trainPredictPlot,testPredictPlot)
+    #plot baseline and predictions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if select=="Risk in Investment":
   st.title("Exploratory Data Analysis of GOOGLE")
   st.header(("Closing Price"))
   fig=plt.figure(figsize=(10,5))
@@ -69,7 +119,12 @@ if select=="Analysis and Risk in Investment":
   st.pyplot(fig)
 
 # Now let's plot the total volume of stock being traded each day
-  st.header("Total Volume of the stock being traded each day")
+  st.header("Volume of the stock being traded per day")
+  st.text('''Average daily trading volume is an important metric because high or
+low trading volume attracts different types of traders and investors.''')
+  st.caption('''Many traders and investors prefer higher average daily trading 
+volume compared to low trading volume, because with high volume it is easier to 
+get into and out positions.''')
   fig=plt.figure(figsize=(10, 5))
   df['Volume'].plot()
   plt.ylabel("Volume")
@@ -77,14 +132,26 @@ if select=="Analysis and Risk in Investment":
   st.pyplot(fig)
 
   st.header("Daily Return of GOOGLE")
-  st.text('''Now that we've done some baseline analysis, let's go ahead and dive 
-  a little deeper. We're now going to analyze the risk of the stock. In order to 
-  do so we'll need to take a closer look at the daily changes of the stock, and 
-  not just its absolute value.''')
+  st.caption('''We're now going to analyze the risk of the stock. In order to 
+do so we'll need to take a closer look at the daily changes of the stock, and 
+not just its absolute value.''')
   fig=plt.figure(figsize=(10, 5))
   df['Daily Return']=df['Close'].pct_change()
   df['Daily Return'].plot(legend=True, linestyle='--', marker='o').set_title('Daily Return of GOOGLE')
   st.pyplot(fig)
+  
+  st.subheader("Histogram of Daily Return of GOOGLE")
+  st.text('''Histogram helps to analyse the stock return and is vely helpful if
+we are comparing the performace of teh stock with other stocks.''')
+  st.caption('''What if there is skewness in the daily return plot:
+
+
+Positive Skewness:the values of the dataset extend to the right of the peak
+A relatively high positive skewness reading indicates returns deep in the right tail of the distribution.
+
+Negative Skewness; the values of the dataset extend to left of the peak
+A negative number equates with a loss in the left tail
+''')
 
   fig=plt.figure(figsize=(10, 5))
   df['Daily Return'].hist(bins=50)
@@ -94,8 +161,8 @@ if select=="Analysis and Risk in Investment":
 
   st.header("How much value do we put at risk by investing in a particular stock?")
   st.text('''There are many ways we can quantify risk, one of the most basic 
-  ways using the information we've gathered on daily percentage returns is by 
-  comparing the expected return with the standard deviation of the daily returns.''')
+ways using the information we've gathered on daily percentage returns is by 
+comparing the expected return with the standard deviation of the daily returns.''')
   rets = df[["Daily Return"]]
 
   area = np.pi * 20
@@ -109,16 +176,12 @@ if select=="Analysis and Risk in Investment":
   plt.ylabel('Risk')
   plt.title("Risk by investing in GOOGLE")
   st.pyplot(fig)
-  data=st.sidebar.selectbox("Analyse Stock",("Moving Average","Seasonality"))
+  
   df=df[['Close']]
-# fig=plt.figure(figsize=(10,5))
-# plt.plot(df)
-# plt.ylabel("Closing Price")
-# plt.xlabel("Date")
-# st.pyplot(fig)
 
-#st.line_chart(df)
-if select=="Invest":
+if select=="Investment Period":
+  df=df[['Close']]
+  data=st.sidebar.selectbox("Time of Investment",("Moving Average","Seasonality"))
   if data=="Moving Average":
     ma100=df.rolling(100).mean()
     ma200=df.rolling(200).mean()
@@ -128,6 +191,10 @@ if select=="Invest":
     constantly updated average price.A rising moving average indicates that the 
     security is in an uptrend, while a declining moving average indicates a 
     downtrend.''')
+    st.markdown("IF 100 MA>200 MA: UPTREND (buy stock).")
+    st.markdown("IF 100 MA<200 MA : DOWNTREND (sell stock)")
+    st.caption(" ")
+    st.caption("The plot below gives you the information when to buy and sella  stock")
     fig=plt.figure(figsize=(10,5))
     plt.plot(df)
     plt.plot(ma100,'red',label="100 day Moving Average")
@@ -137,22 +204,24 @@ if select=="Invest":
     plt.ylabel("Closing Price")
     plt.legend()
     st.pyplot(fig)
-    st.markdown("IF 100 MA>200 MA: UPTREND (buy stock).")
-    st.markdown("IF 100 MA<200 MA : DOWNTREND (sell stock)")
 
   if data=="Seasonality":
     st.header("Seasonality")
-    st.text('''Seasonality is the study of a market over time to establish if 
-    there is a regular and predictable change that occurs within that markets 
-    price every calendar year.Every market experiences periods of either greater 
-    supply or demand throughout a year, and it is these forces that drive 
-    seasonal patterns.''')
+    st.text('''Seasonality is the study of a market over time to establish if there
+is a regular and predictable change that occurs within that markets price every 
+calendar year.Every market experiences periods of either greater 
+supply or demand throughout a year, and it is these forces that drive seasonal patterns.''')
+    st.caption("")
+    st.caption('''It is recommened to observe the seasonality plot, and invest in the stock
+    on that basis''')
     result = seasonal_decompose(df, model='multiplicative', period=365)
     fig = result.plot()
+    
     plt.legend()
     st.pyplot(fig)
+
 df=df[['Close']]
-if select=='Model':
+if select=='Model and Accuracy':
   st.title("Time Series Forecasting")
   model=st.sidebar.selectbox("Model:",("ARIMA: z-score","ARIMA: log","SVR","LSTM"))
 
@@ -213,44 +282,44 @@ if select=='Model':
 
   if model=="LSTM":
     st.header("LSTM")
-    
-    scaler=MinMaxScaler(feature_range=(0,1))
-    df1=scaler.fit_transform(np.array(df).reshape(-1,1))
-    train_size=int(len(df1)*0.65)
-    test_size=len(df1)-train_size
-    train_data,test_data=df1[0:train_size,:],df1[train_size:len(df1),:1]
-    def create_dataset(dataset,time_step=1):
-      dataX,dataY= [],[]
-      for i in range(len(dataset)-time_step-1):
-        a=dataset[i:(i+time_step),0]
-        dataX.append(a)
-        dataY.append(dataset[i+time_step,0])
-      return np.array(dataX), np.array(dataY)
-    time_step=100
-    X_train,y_train= create_dataset(train_data,time_step)
-    X_test,y_test=create_dataset(test_data,time_step)
-    X_train= X_train.reshape(X_train.shape[0],X_train.shape[1],1)
-    X_test= X_test.reshape(X_test.shape[0],X_test.shape[1], 1)
-    model=Sequential()
-    model.add(LSTM(100, return_sequences=True, input_shape=(100,1)))
-    model.add(LSTM(1000,return_sequences=False))
-    model.add(Dense(25))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error',optimizer='adam')
-    model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=10,batch_size=64,verbose=1)
-    train_predict=model.predict(X_train)
-    test_predict=model.predict(X_test)
-    train_predict=scaler.inverse_transform(train_predict)
-    test_predict=scaler.inverse_transform(test_predict)
-    look_back=100
-    trainPredictPlot=np.empty_like(df1)
-    trainPredictPlot[:,:]=np.nan
-    trainPredictPlot[look_back:len(train_predict)+look_back,:]=train_predict
-    # shift test predictions for plotting
-    testPredictPlot=np.empty_like(df1)
-    testPredictPlot[:,:]=np.nan
-    testPredictPlot[len(train_predict)+(look_back*2)+1:len(df1)-1,:]=test_predict
-    # #plot baseline and predictions
+    # scaler=MinMaxScaler(feature_range=(0,1))
+    # df1=scaler.fit_transform(np.array(df).reshape(-1,1))
+    # train_size=int(len(df1)*0.65)
+    # test_size=len(df1)-train_size
+    # train_data,test_data=df1[0:train_size,:],df1[train_size:len(df1),:1]
+    # def create_dataset(dataset,time_step=1):
+    #   dataX,dataY= [],[]
+    #   for i in range(len(dataset)-time_step-1):
+    #     a=dataset[i:(i+time_step),0]
+    #     dataX.append(a)
+    #     dataY.append(dataset[i+time_step,0])
+    #   return np.array(dataX), np.array(dataY)
+    # time_step=100
+    # X_train,y_train= create_dataset(train_data,time_step)
+    # X_test,y_test=create_dataset(test_data,time_step)
+    # X_train= X_train.reshape(X_train.shape[0],X_train.shape[1],1)
+    # X_test= X_test.reshape(X_test.shape[0],X_test.shape[1], 1)
+    # model=Sequential()
+    # model.add(LSTM(100, return_sequences=True, input_shape=(100,1)))
+    # model.add(LSTM(1000,return_sequences=False))
+    # model.add(Dense(25))
+    # model.add(Dense(1))
+    # model.compile(loss='mean_squared_error',optimizer='adam')
+    # model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=10,batch_size=64,verbose=1)
+    # train_predict=model.predict(X_train)
+    # test_predict=model.predict(X_test)
+    # train_predict=scaler.inverse_transform(train_predict)
+    # test_predict=scaler.inverse_transform(test_predict)
+    # look_back=100
+    # trainPredictPlot=np.empty_like(df1)
+    # trainPredictPlot[:,:]=np.nan
+    # trainPredictPlot[look_back:len(train_predict)+look_back,:]=train_predict
+    # # shift test predictions for plotting
+    # testPredictPlot=np.empty_like(df1)
+    # testPredictPlot[:,:]=np.nan
+    # testPredictPlot[len(train_predict)+(look_back*2)+1:len(df1)-1,:]=test_predict
+    # # #plot baseline and predictions
+    df1,trainPredictPlot,testPredictPlot=lstm(df)
     fig=plt.figure(figsize=(10,5))
     plt.plot(scaler.inverse_transform(df1))
     plt.xlabel("Days")
